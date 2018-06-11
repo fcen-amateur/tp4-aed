38fe8b7a2b79b383d77a6895862b567362ff2894
Juan Manuel Berros <juanmaberros@gmail.com>
2018-06-11 15:35:14 -0300

    WIP k-vecinos, mejora algunos nombres

--- k_vecinos.R
+++ k_vecinos.R
@@ -1,13 +1,11 @@
-k_vecinos <- function(formula, train_df, k, weighted_vote = F) {
+k_vecinos <- function(formula, train_df, k) {
   #' Entrena un predictor de K-vecinos más cercanos a partir de todos los datos
-  #' de `train_df`.
+  #' de `train_df`. Las variables predictoras y la variable a estimar son
+  #' tomadas de la `formula`.
   #'
-  #' Escala todas las variables centrando cada una en su media y dividiendo
-  #'  por el desvío estándar.
-  #'
-  #' Por defecto, gana la categoría que obtiene el "majority vote".
-  #' Si `weigthed_vote` = T, los `k` vecinos más cercanos aportan un voto de
-  #' peso inversamente proporcional a la distancia a la observación.
+  #' Todas las variables predictoras son escaladas, centrando cada una en su
+  #' media y dividiendo por el desvío estándar. Las observaciones también
+  #' son escaladas antes de la clasificación.
   #'
   #' Devuelve una lista con:
   #'  - predictor (funcion): Función que dado un df de observaciones no
@@ -15,7 +13,7 @@ k_vecinos <- function(formula, train_df, k, weighted_vote = F) {
   #'      de predicciones.
 
   # Extraemos los nombres de variables de la `formula`
-  agregar_sufijo <- function (s) { glue("{s}1") }
+  agregar_sufijo <- function (s) { paste(s, "1", sep="") }
   var_y <- all.vars(formula)[1]
   vars_x <- all.vars(formula)[-1]
   var_y_con_sufijo <- agregar_sufijo(var_y)
@@ -25,60 +23,56 @@ k_vecinos <- function(formula, train_df, k, weighted_vote = F) {
   # las nuevas observaciones antes de clasificarlas:
   medias <- list()
   desvios <- list()
-  train_df_escalado <- select(train_df, vars_x, var_y) # Y como última columna
+  train_df_escalado <- select_(train_df, var_y)
   for (var_x in vars_x) {
     x <- train_df[[var_x]]
-    media <- mean(x)
-    desvio <- sd(x)
-
-    medias[[var_x]] <- media
-    desvios[[var_x]] <- desvio
-    train_df_escalado[[var_x]] <- ((x - media) / desvio)
+    medias[[var_x]] <- mean(x)
+    desvios[[var_x]] <- sd(x)
+    train_df_escalado[[var_x]] <- ((x - medias[[var_x]]) / desvios[[var_x]])
   }
 
   # Construimos la función `predictor`, que dado un `test_df` usa al
   # `train_df` para clasificar cada observación.
   predictor <- function(test_df) {
-    test_df[[var_y]] <- NA
-    test_df_escalado <- select(test_df, vars_x, var_y)
     # Escalamos cada nueva observación con media y sd de los datos de training
+    test_df_escalado <- select(test_df, vars_x)
     for (var_x in vars_x) {
-      test_df_escalado[[var_x]] <- (
-        (test_df[[var_x]] - medias[[var_x]]) / desvios[[var_x]]
-      )
+      serie_escalada <- (test_df[[var_x]] - medias[[var_x]]) / desvios[[var_x]]
+      test_df_escalado[[var_x]] <- serie_escalada
     }
+    test_df_escalado[[var_y]] <- NA
+
     # Cruzamos cada nueva observación con cada dato de training
     distancias <-
       test_df_escalado %>%
       rowid_to_column("obs_id") %>%
       crossing(train_df_escalado)
 
-    calcular_distancia <- function(test_df_row) {
-      dato_nuevo <- test_df_row[vars_x]
-      # Las columnas de training recibieron sufijo "1":
-      dato_train <- test_df_row[vars_x_con_sufijo]
-      dist(rbind(dato_nuevo, dato_train))[[1]]
+    calcular_distancia <- function(obs_vs_dato) {
+      observacion <- obs_vs_dato[vars_x]
+      # Las columnas de training recibieron el sufijo "1":
+      dato_train <- obs_vs_dato[vars_x_con_sufijo]
+      dist(rbind(observacion, dato_train), method = "euclidean")[[1]]
     }
 
     distancias[["distancia"]] <- apply(distancias, 1, calcular_distancia)
-    distancias[["peso"]] <- 1/distancias[["distancia"]]
 
-    categorias <-
+    vecinos_mas_cercanos <-
       distancias %>%
       group_by(obs_id) %>%
-      top_n(-k, distancia) %>%
+      top_n(-k, distancia) %>% # Incluye empates!
+      arrange(obs_id, distancia)
+
+    votos <-
+      vecinos_mas_cercanos %>%
       group_by_("obs_id", var_y_con_sufijo) %>%
-      summarise(votes = n(), weighted_votes = sum(peso)) %>%
+      summarise(votes = n()) %>%
       arrange(obs_id)
 
-    # FIXME: la version weighted_vote tiene Inf colados cuando la distancia es 0
-    # FIXME: la version !weighted_vote no resuelve los empates.
+    # FIXME: no resuelve los empates.
     # TODO: Comparar con una versión enlatada de knn
-    if (weighted_vote) {
-      result <- top_n(categorias, 1, weighted_votes)[[var_y_con_sufijo]]
-    } else {
-      result <- top_n(categorias, 1, votes)[[var_y_con_sufijo]]
-    }
+    result <- top_n(categorias, 1, votes)[[var_y_con_sufijo]]
+
     return (result)
   }
 
